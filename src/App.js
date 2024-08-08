244cc4445aa999e5a10f38832174426ab5eb19dc
EkateriniTri <136068628+EkateriniTri@users.noreply.github.com>
2024-08-08 17:52:59 +0200

    correction on the Weather.js and Search.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
-import Weather from "./Weather";
-
 import "./App.css";
+import Search from "./Search";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <Weather city="Athens" />
-      </header>
+      <h1>City Weather</h1>
+      <Search />
+
+      <footer>
+        This project was coded by Katerina Tri and is{" "}
+        <a
+          href="https://github.com/EkateriniTri/weather-react.git"
+          target="_blank">
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://earnest-croquembouche-925bd3.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer">
+          {" "}
+          hosted on Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,33 @@
-import React from "react";
 import axios from "axios";
+import React from "react";
+import { useState } from "react";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.city} is ${response.data.temperature.current}C`
-    );
+export default function (props) {
+  let [temp, setTemp] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [icon, setIcon] = useState(null);
+
+  function showWeather(response) {
+    console.log(response.data);
+    setTemp(Math.round(response.data.main.temperature.current));
+    setDescription(response.condition.description);
+    setWind(response.wind.speed);
+    setHumidity(response.temperature.humidity);
+    setIcon(response.condition.icon_url);
   }
 
-  let apiKey = "6c4a1fa1oe455fb969c4b3876f90t341";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  let url = `https://api.shecodes.io/weather/v1/current?query=${props.city}&appid=6c4a1fa1oe455fb969c4b3876f90t341&units=metric`;
+  axios.get(url).then(showWeather);
+
+  return (
+    <ul>
+      <li>Temperature: {temp}°C</li>
+      <li>Description: {description}</li>
+      <li>Humidity: {humidity}%</li>
+      <li>Wind: {wind}km/h</li>
+      <li>{icon}</li>
+    </ul>
+  );
 }
